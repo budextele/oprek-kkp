ce87346f4d03c1c7cd1dad36ce65f37d4844da19
budextele <wahyubudi902@gmail.com>
2022-07-22 10:59:54 +0700

    first commit

---
+++ src/oprek_kkp/Oprek_kkp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oprek_kkp;
+
+import login_akun.loginPage;
+
+/**
+ *
+ * @author Budi Utomo
+ */
+public class Oprek_kkp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        loginPage login = new loginPage();
+        login.setVisible(true);
+    }
+
+}
