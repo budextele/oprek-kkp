a7a1ecb68f364d90ba5071d575c2e05708ed8001
budextele <wahyubudi902@gmail.com>
2022-07-24 18:23:39 +0700

    rename project & penamaan ulang class , package

---
+++ src/modul/koneksi.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul;
+
+//import project.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Budi Utomo
+ */
+public class koneksi {
+    private static Connection mySQLConnection;
+
+    public static Connection getKoneksi() throws SQLException {
+        try {
+            // openconnection
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            // database - oprek_kkp
+            // username - admin
+            // password - admin
+            String urlDB = "jdbc:mysql://localhost:3306/oprek_kkp?useSSL=false";
+            String userDB = "admin";
+            String passDB = "admin";
+            mySQLConnection = DriverManager.getConnection(urlDB, userDB, passDB);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+        return mySQLConnection;
+    }
+}
